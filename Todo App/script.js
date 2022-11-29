b25e9d836b69ce2858bb1f9c3a1d1e0750f32022
binisnull <dbsqlssss0704@gmail.com>
2022-11-29 14:48:33 +0900

    Todo App

---
+++ Todo App/script.js
@@ -0,0 +1,20 @@
+const form  = document.getElementById('form');
+const input = document.getElementById('input');
+const todos = document.getElementById('todos');
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    const todoText = input.value;
+
+    if(todoText){
+        const todoE1 = document.createElement('li');
+        todoE1.innerText = todoText;
+
+        todoE1.addEventListener('click', () =>{
+            todoE1.classList.toggle('completed');
+        })
+        todos.appendChild(todoE1);
+
+        input.value = "";
+    }
+});

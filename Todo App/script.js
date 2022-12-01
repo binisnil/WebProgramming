80cc3a8516070f94160dba6a7f84f71e2f26310f
binisnull <dbsqlssss0704@gmail.com>
2022-12-01 20:07:43 +0900

    add: right click 시 TodoItem 삭제 기능

--- Todo App/script.js
+++ Todo App/script.js
@@ -4,6 +4,11 @@ const todos = document.getElementById('todos');
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
+    addTodo();
+
+});
+function addTodo() {
+    
     const todoText = input.value;
 
     if(todoText){
@@ -13,8 +18,30 @@ form.addEventListener("submit", (e) =>{
         todoE1.addEventListener('click', () =>{
             todoE1.classList.toggle('completed');
         })
+
+        todoE1.addEventListener('contextmenu', (e) =>{
+            e.preventDefault();
+
+            todoE1.remove();
+        });
+        
         todos.appendChild(todoE1);
 
         input.value = "";
     }
-});
+}
+
+function updateLS() {
+    const todosE1 = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosE1.forEach(todoE1 => {
+        notes.push({
+            text: todoE1.innerText,
+            completed: todoE1.classList.contains("completed"),
+        })
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
